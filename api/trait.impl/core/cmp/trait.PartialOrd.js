bd6cfb72b6dae510d0b2e7a2a7e0473ba865d8bb
N0zoM1z0 <N0zoM1z0@users.noreply.github.com>
2026-07-17 03:46:07 +0000

    deploy: 78dad5e91f97a95c19a5f33a52fdad7b7dc50031

---
+++ api/trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["leantoken",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"leantoken/services/context/facets/enum.FacetKind.html\" title=\"enum leantoken::services::context::facets::FacetKind\">FacetKind</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[333]}
