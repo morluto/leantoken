c4ac1130e06ca44b51959b6356deeb41b198112f
morluto <morluto@users.noreply.github.com>
2026-07-29 14:19:47 +0000

    deploy: 5acd64c06558dd0077c3a73b8ba6c965d8013a36

--- api/leantoken/error/sidebar-items.js
+++ api/leantoken/error/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Error","IndexLimitKind","RetryableOperation"],"struct":["InputViolation","InputViolations","ResponseBudgetBreakdown"],"type":["Result"]};
+window.SIDEBAR_ITEMS = {"enum":["Error","IndexLimitKind","RetrievalLimitKind","RetryableOperation"],"struct":["InputViolation","InputViolations","ResponseBudgetBreakdown"],"type":["Result"]};
